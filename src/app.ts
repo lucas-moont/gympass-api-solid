8c2519bcf5f0a1e5629201ac8fca7030e004c0bf
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-04-23 00:04:54 -0300

    --feat Adding users is now possible.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'crypto'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  })
+
+  const {name, email, password} = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password
+    }
+  })
+
+  return reply.status(201).send()
+})
