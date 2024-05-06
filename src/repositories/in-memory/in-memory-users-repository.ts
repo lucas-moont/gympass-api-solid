111f8d758c11b9fe674cbd6348c0edc77aac6cc1
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-05-05 23:20:17 -0300

    --feat In memory repository has findByEmail and create methods functionalities.

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -2,17 +2,27 @@ import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+  
   async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find(item => item.email === email)
+    if(user !== undefined){
+      return user 
+    }
     return null
   }
 
   async create(data: Prisma.UserCreateInput): Promise<User> {
-    return {
+    const user = {
       id: 'user-test',
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
       created_at: new Date()
     }
+
+    this.items.push(user)
+
+    return user
   }
 }
