ddbdef047c6f5e8047debc5dbdbc90e61c2381dd
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-05-05 23:35:59 -0300

    --feat Testing user not being able to register with the same email twice.

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,24 +1,44 @@
-import {expect, describe, test} from 'vitest'
-import { RegisterUserService } from './register'
-import { compare } from 'bcryptjs'
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { expect, describe, test } from "vitest";
+import { RegisterUserService } from "./register";
+import { compare } from "bcryptjs";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { UserAlreadyExists } from "./errors/user-already-exists-ts";
 
-describe('Register Use Case', () => {
-  test('should hash user password upon registration', async () => {
-    const inMemoryUserRepo = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUserService(inMemoryUserRepo)
+describe("Register Use Case", () => {
+  test("should hash user password upon registration", async () => {
+    const inMemoryUserRepo = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUserService(inMemoryUserRepo);
 
-    const {user} = await registerUseCase.execute({
-      name: 'John Doe',
-      email: 'johndoe@email.com',
-      password: 'kazuofilhodaputa'
-    })
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password: "kazuofilhodaputa",
+    });
 
     const isPasswordCorrectlyHashed = await compare(
-      'kazuofilhodaputa',
+      "kazuofilhodaputa",
       user.password_hash
-    )
+    );
 
-    expect(isPasswordCorrectlyHashed).toBe(true)
-  })
-})
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  test("should not be able to register with same email twice", async () => {
+    const inMemoryUserRepo = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUserService(inMemoryUserRepo);
+
+    await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password: "kazuofilhodaputa",
+    });
+
+    expect(() =>
+      registerUseCase.execute({
+        name: "John Doe",
+        email: "johndoe@email.com",
+        password: "kazuofilhodaputa",
+      })
+    ).rejects.toBeInstanceOf(UserAlreadyExists); //podemos usar resolve caso esperemos que o teste/promise seja certo
+  });
+});
