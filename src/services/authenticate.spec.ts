da8aa566606157890c1f0e59aa9a49358c0d91dd
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-05-10 14:19:42 -0300

    --feat Testing if user cannot access without the correct credentials.

--- src/services/authenticate.spec.ts
+++ src/services/authenticate.spec.ts
@@ -2,9 +2,10 @@ import { expect, describe, test } from "vitest";
 import { compare, hash } from "bcryptjs";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { AuthenticateService } from "./authenticate";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
 
 describe("Authenticate Service", () => {
-  test("if user chan authenticate sucessfully", async () => {
+  test("if user can authenticate sucessfully", async () => {
     const inMemoryUserRepo = new InMemoryUsersRepository();
     const sut = new AuthenticateService(inMemoryUserRepo);
 
@@ -20,5 +21,23 @@ describe("Authenticate Service", () => {
     });
 
     expect(user.id).toEqual(expect.any(String));
+  });
+
+  test("if user cannot authenticate with invalid credentials", async () => {
+    const inMemoryUserRepo = new InMemoryUsersRepository();
+    const sut = new AuthenticateService(inMemoryUserRepo);
+
+    await inMemoryUserRepo.create({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password_hash: await hash("123456", 6)
+    })
+
+    await expect(async () => {
+      await sut.execute({
+        username: "johndoe@email.com",
+        password: '12345'      
+      });
+    }).rejects.toBeInstanceOf(InvalidCredentialsError)
   });
 });
