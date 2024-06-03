78292ad0b4d991560e93d9d5058e04159184f069
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-03 10:37:54 -0300

    --fix Adding prisma gym repo file.

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,56 @@
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymRepository } from '../gym-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async findByCnpj(cnpj: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        cnpj,
+      },
+    })
+
+    return gym
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gym
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  async findGymByName(gymName: string, page: number) {
+    const gyms = prisma.gym.findMany({
+      where: {
+        title: {
+          contains: gymName,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+}
