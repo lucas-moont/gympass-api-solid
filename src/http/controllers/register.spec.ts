0f36ba7275534582078d8673d9da8ebc2f91fc58
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-14 16:37:10 -0300

    --feat Testing e2e register.

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,22 @@
-import { test } from "vitest";
+import { test, describe, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {
-  
+describe('e2e register testing', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  test('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
