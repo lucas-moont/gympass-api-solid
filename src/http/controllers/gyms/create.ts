504aa6b173add52e2c6bb84eed6bd7b9fe5c0cdf
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-14 21:54:28 -0300

    --feat Adding gym create controller.

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeCreateGym } from '@/services/factories/make-create-gym-service'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    cnpj: z.string(),
+    description: z.string().nullable(),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+    phone: z.string(),
+  })
+
+  const { title, cnpj, description, latitude, longitude, phone } =
+    createGymBodySchema.parse(request.body)
+
+  const createGym = makeCreateGym()
+  await createGym.execute({
+    title,
+    cnpj,
+    description,
+    latitude,
+    longitude,
+    phone,
+  })
+
+  return reply.status(201).send()
+}
