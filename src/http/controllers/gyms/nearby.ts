d14601be1271e5ae992b26ca232cca5f10d08c4a
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-14 22:19:55 -0300

    --feat Adding gym search and nearby controllers and also adding its proper routes to the responsible file.

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,29 @@
+import { makeFetchNearbyGymsService } from '@/services/factories/make-fetch-nearby-gyms-service'
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function nearby(req: FastifyRequest, reply: FastifyReply) {
+  const nearbyQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { latitude, longitude, page } = nearbyQuerySchema.parse(req.body)
+
+  const nearbyGymsService = makeFetchNearbyGymsService()
+
+  const gyms = await nearbyGymsService.execute({
+    userLongitude: longitude,
+    userLatitude: latitude,
+    page,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,23 @@
+import { makeSearchGymsService } from '@/services/factories/make-search-gym'
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function search(req: FastifyRequest, reply: FastifyReply) {
+  const searchQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = await searchQuerySchema.parse(req.body)
+
+  const searchGymService = makeSearchGymsService()
+
+  const gyms = await searchGymService.execute({
+    query,
+    page,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
