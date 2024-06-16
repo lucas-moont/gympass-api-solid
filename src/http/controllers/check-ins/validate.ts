28878d3223e700239f45902b7bd490e285aa25e5
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-15 21:12:36 -0300

    --feat Validation controller for check ins functionallity.

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+import { makeValidateCheckInsService } from '@/services/factories/make-validate-check-in'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function validate(req: FastifyRequest, reply: FastifyReply) {
+  const validateCheckInsQuerySchema = z.object({
+    checkinId: z.string().uuid(),
+  })
+
+  const { checkinId } = validateCheckInsQuerySchema.parse(req.body)
+
+  const validateCheckInService = makeValidateCheckInsService()
+
+  await validateCheckInService.execute({
+    checkinId,
+  })
+
+  reply.status(204).send()
+}
