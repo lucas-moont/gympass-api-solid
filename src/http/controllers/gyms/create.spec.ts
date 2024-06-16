a9fdb6ac809e3ad6b56986414cfcd4f475297d38
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-15 23:59:49 -0300

    --feat Adding create gym e2e testing.

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,32 @@
+import { app } from '@/app'
+import { beforeAll, afterAll, test, describe, expect } from 'vitest'
+import request from 'supertest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('create gyms testing (e2e)', () => {
+  beforeAll(async () => {
+    app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  test('should be able to create gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Nós é maluco',
+        cnpj: '123456789',
+        phone: '123456789',
+        latitude: -22.8506215,
+        longitude: -43.0874256,
+        description: '',
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
