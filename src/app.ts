1e316c3c6fd86f4f25bb68d34f4bffafa770e55b
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-16 00:01:27 -0300

    --fix Adding check in routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ app.register(fastifyJwt, {
 
 app.register(userRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
