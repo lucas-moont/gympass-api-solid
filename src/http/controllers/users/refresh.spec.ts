0392f8f80daab1a9957991cf3d7ec6551cb12ee0
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-22 00:48:22 -0300

    --feat Adds refresh token testing and done some minor fixes on package.json and vitest version in order to run E2E tests quicker.

---
+++ src/http/controllers/users/refresh.spec.ts
@@ -0,0 +1,41 @@
+import { describe, test, beforeAll, afterAll, expect } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('refresh token E2E testing', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should be able to refresh token', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/session').send({
+      username: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    const cookies = authResponse.get('Set-Cookie') ?? []
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken'),
+    ])
+  })
+})
