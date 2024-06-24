a3c22349dc15a312fbc652cf023d2e412331d04b
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-06-23 22:13:41 -0300

    --feat Adding role verification middleware.

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { history } from './history'
 import { metrics } from './metrics'
 import { validate } from './validate'
 import { create } from './create'
+import { verifyUserRole } from '@/http/middlewares/verifyUserRole'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
@@ -12,5 +13,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/:checkinId/validate', validate)
+  app.patch(
+    '/check-ins/:checkinId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
